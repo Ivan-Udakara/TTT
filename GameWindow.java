f9e112aeaa0528865667b99cd157a79620c728a4
Ivan Udakara <ivanud.12@cse.mrt.ac.lk>
2014-10-11 16:17:23 +0530

    Create GameWindow.java

---
+++ GameWindow.java
@@ -0,0 +1,10 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class GameWindow {
+	JFrame gameWindowFrm = new JFrame();
+}
